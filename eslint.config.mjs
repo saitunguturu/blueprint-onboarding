b429d9d0cd4f9dafd3dcd0b3041eb28de80fa919
Sai Divyesh Tunguturu <67570329+saitunguturu@users.noreply.github.com>
2025-09-23 22:29:51 -0700

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    extends: compat.extends(
+      'next/core-web-vitals',
+      'next/typescript',
+      'prettier',
+    ),
+    ignores: ['**/node_modules/**', '**/.next/**'],
+    rules: {
+      '@next/next/no-duplicate-head': 'off',
+    },
+  },
+]);
